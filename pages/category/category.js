b7bdf8751526325fb5049861992c43acefa419d4
HuanqingDeng <1145349052@qq.com>
2018-05-30 10:04:45 +0800

    wangyiYanXuan

---
+++ pages/category/category.js
@@ -0,0 +1,85 @@
+// pages/category/category.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    category:[
+      {name:'新品',id:'newPro'},
+      {name:'手机',id:'phone'},
+      {name:'电视',id:'TV'},
+      {name:'电脑',id:'pc'},
+      {name:'家电',id:'jiadian'},
+      {name:'家装',id:'jiazhuang'},
+      {name:'路由',id:'luyou'},
+      {name:'智能',id:'zhineng'},
+      {name:'儿童',id:'chid'},
+      {name:'灯具',id:'dengju'},
+      {name:'电源',id:'dianyuan'},
+      {name:'耳机',id:'erji'},
+      {name:'音响',id:'yinxiang'},
+      {name:'礼品',id:'lipin'},
+      {name:'生活',id:'life'},
+      {name:'服务',id:'server'},
+      {name:'米粉卡',id:'Mifen'}
+      
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
